9a47d007f9cb62664c95eb739accaa8d5997809a
scb2scb2 <daniel@xs4all.nl>
2014-06-16 11:48:35 +0300

    moved these classes from noterik.* package to springfield.org

---
+++ src/org/springfield/lou/fs/IncorrectFilterException.java
@@ -0,0 +1,19 @@
+package org.springfield.lou.fs;
+
+public class IncorrectFilterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7812233180351063365L;
+
+	public IncorrectFilterException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IncorrectFilterException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
