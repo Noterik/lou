5b74c1ff00c2c1424a528d9abff7014a8ea627ec
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2014-08-20 14:08:20 +0200

    fixed null pointer exception

--- src/org/springfield/lou/servlet/LouServlet.java
+++ src/org/springfield/lou/servlet/LouServlet.java
@@ -285,7 +285,7 @@ public class LouServlet extends HttpServlet {
 			
 			System.out.println("USER-AGENT="+request.getHeader("user-agent"));
 			String agent = request.getHeader("user-agent");
-			if (agent.indexOf("HbbTV/1.1.1")==-1) {
+			if (agent != null && agent.indexOf("HbbTV/1.1.1")==-1) {
 				body+="<script language=\"javascript\" type=\"text/javascript\" src=\"/eddie/js/eddie.js?cache\"></script>\n";
 				body+="<script language=\"javascript\" type=\"text/javascript\" src=\"/eddie/js/main.js\"></script>\n";
 				body+="<script language=\"javascript\" type=\"text/javascript\" src=\"/eddie/js/stacktrace.js\"></script>\n";
