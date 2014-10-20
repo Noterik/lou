6576c46802317cc8a3edebb65d180ef5d2fe6102
David Ammeraal <david.ammeraal@gmail.com>
2014-10-20 12:11:15 +0200

    Fixed console.log bug for IE9
    -If window.console is not available, make it and make an empty
    console.log() method. In IE9 console is not supported i if the debugger
    is not opened.

--- src/org/springfield/lou/servlet/LouServlet.java
+++ src/org/springfield/lou/servlet/LouServlet.java
@@ -228,6 +228,7 @@ public class LouServlet extends HttpServlet {
 			body+="<meta name=\"apple-mobile-web-app-status-bar-style\" content=\"black\" />";
 			body+="<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n";
 			body+="<meta name=\"viewport\" content=\"width=device-width, user-scalable = no,initial-scale=1.0; maximum-scale=1.0;\">";
+			body+="<script type=\"text/javascript\">if (!window.console) window.console = {};if (!window.console.log) window.console.log = function () { };</script>";
 			body+="<script language=\"javascript\" type=\"text/javascript\">var LouSettings = {\"lou_ip\": \"" + LazyHomer.getExternalIpNumber() + "\", \"lou_port\": \"" + LazyHomer.getBartPort() + "\", \"user\": \"" + user + "\", \"app\": \"" + nameapp + "\", \"fullapp\": \"" + fullappname + "\", \"appparams\": \"" + params + "\"}</script>\n";
 			body+="<script language=\"javascript\" type=\"text/javascript\" src=\"/eddie/js/jquery-1.8.0.js\"></script>\n";
 			body+="<script language=\"javascript\" type=\"text/javascript\" src=\"/eddie/js/jquery.xdomainrequest.min.js\"></script>\n";
