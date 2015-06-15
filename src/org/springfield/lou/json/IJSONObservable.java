0a7b798eedeef1edf048d57cbd1e185ed9994125
DavidAmmeraal <david.ammeraal@gmail.com>
2015-06-15 14:06:54 +0200

    Added getObservers method

--- src/org/springfield/lou/json/IJSONObservable.java
+++ src/org/springfield/lou/json/IJSONObservable.java
@@ -1,10 +1,12 @@
 package org.springfield.lou.json;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IJSONObservable {
 	public void update();
 	public void update(String field);
 	public void update(ArrayList<String> field);
 	public void addObserver(IJSONObserver observer);
+	public List<IJSONObserver> getObservers();
 }
